638d401358a2e5b6aa793013a10e14d850bed1c2
nixeast <eastnix@gmail.com>
2024-05-27 22:17:42 +0900

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject btnTicTacToe_01;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("Hello world!!");
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TestMessage()
+    {
+        Debug.Log("test btn clicked..");
+    }
+}
