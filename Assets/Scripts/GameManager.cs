d1f4e276f1a029110b895650101083be6a03fe63
nixeast <eastnix@gmail.com>
2024-05-28 15:49:07 +0900

    update button click function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
-    public GameObject btnTicTacToe_01;
+    //public GameObject btnTicTacToe_01;
+
+    //int sizeTicTacToeBtn = 0;
+    //sizeTicTacToeBtn = 9;
+    public GameObject[] btnTicTacToe;
+    //int nCurrentPlayerNumber = 0;
+    TMP_Text test_tmpText;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -13,7 +22,8 @@ public class GameManager : MonoBehaviour
 
         Debug.Log("Hello world!!");
 
-        
+        string strBtnName = btnTicTacToe[0].name;
+        Debug.Log(strBtnName);
 
     }
 
@@ -22,8 +32,23 @@ public class GameManager : MonoBehaviour
     {
         
     }
-    public void TestMessage()
+
+    public void TestMessage(int nBtnNumber)
     {
-        Debug.Log("test btn clicked..");
+        //string strMyName = this.name;
+        //Debug.Log("test btn clicked..");
+        Debug.Log("btn " + nBtnNumber +" clicked..");
+
+        string tmp = btnTicTacToe[nBtnNumber].transform.GetChild(0).name;
+
+        //Text txtBtnNumber = btnTicTacToe[nBtnNumber].GetComponent<Text>();
+        Debug.Log(tmp);
+
+        GameObject testGameObject = btnTicTacToe[nBtnNumber].transform.GetChild(0).gameObject;
+        string myString = testGameObject.GetComponent<TMP_Text>().text;
+
+        Debug.Log(myString);
+
     }
+
 }
